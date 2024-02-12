aa561c51386a3087b9f6da36bb654d11a7c0614f
Androtim <143963999+Androtim@users.noreply.github.com>
2024-02-12 11:19:46 +0100

    Update create_base_window.py
    made interactive

--- Licornia-Tim/For class/create_base_window.py
+++ Licornia-Tim/For class/create_base_window.py
@@ -7,11 +7,17 @@ pygame.init()
 fenetre = pygame.display.set_mode((800, 600))
 
 #Chargement et collage du fond
-eau = pygame.image.load("eau.png").convert()
-terre = pygame.image.load("terre.png").convert()
 fond = pygame.image.load("background.jpg").convert()
 fenetre.blit(fond, (0, 0))
 
+#chargement des sprites
+eau = pygame.image.load("eau.png").convert()
+eau_x = -1000
+eau_y = -1000
+
+terre = pygame.image.load("terre.png").convert()
+terre_x = -1000
+terre_y = -1000
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
@@ -22,12 +28,16 @@ while continuer :
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
-        if event.type == MOUSEBUTTONDOWN and event.button == 1:
-            fenetre.blit(eau, (320, 260))
-        if event.type == MOUSEBUTTONDOWN and event.button == 3:
-            fenetre.blit(terre, (400, 260))
+        if event.type == MOUSEBUTTONDOW:
+            if event.button == 1:
+            	eau_x = 320
+            	eau_y = 260
+        	if event.button == 3:
+            	terre_x = 400
+            	terre_y = 260
 
-        if event.type == KEYDOWN and event.key == K_SPACE:
-        	print("Espace")
+	fenetre.blit(eau, (eau_x, eau,y))
+	fenetre.blit(terre, (terre_x, terre_y))
+	pygame.display.flip()
 
 pygame.quit()
